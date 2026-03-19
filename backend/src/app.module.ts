14df5d5b4559927f2bd6d348ff916f6b87772bd8
Yorjas4 <yor_jas@hotmail.com>
2026-03-19 12:46:50 -0600

    comisiones

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ComisionesModule } from './comisiones/comisiones.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { SequelizeModule } from '@nestjs/sequelize';
       database: 'test',
       models: [],
     }),
+    ComisionesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
