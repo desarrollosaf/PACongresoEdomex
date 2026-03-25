af8f312d4438c4fa7a07fca71bf870d098f322dd
MarSGlez <martin.sanchezg@hotmail.com>
2026-03-24 19:13:14 -0600

    cambios moduel trabajo legislativo

--- backend/src/trabajo_legislativo/trabajo_legislativo.module.ts
+++ backend/src/trabajo_legislativo/trabajo_legislativo.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TrabajoLegislativoService } from './trabajo_legislativo.service';
 import { TrabajoLegislativoController } from './trabajo_legislativo.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { SequelizeModule } from '@nestjs/sequelize/dist/sequelize.module';
 
 @Module({
+  imports: [
+      SequelizeModule.forFeature([
+        DatabaseModule
+      ]),
+    ],
   controllers: [TrabajoLegislativoController],
   providers: [TrabajoLegislativoService],
 })
