e8cf2af549661546f5a45cadb388a9eb0b42d609
cescDG <cesar199403@hotmail.com>
2026-03-25 12:01:50 -0600

    dd

--- src/app/service/boletines.api.ts
+++ src/app/service/boletines.api.ts
@@ -31,3 +31,20 @@ export async function getBoletinesRandom() {
     return [];
   }
 }
+
+export async function getBoletines() {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/boletines`, { next: { revalidate: 10 } });
+    if (!res.ok) {
+      const text = await res.text();
+      console.error('Error backend:', text);
+      return [];
+    }
+  return await res.json();
+  } catch (error) {
+    console.error("Failed to fetch boletines:", error);
+    return [];
+  }
+}
+
+
