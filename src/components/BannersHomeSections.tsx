3ebe61897e8ba78e8fec12112643515ed5f6ef97
GiselDiaz <gis_ale13@hotmail.com>
2026-03-26 16:38:32 -0600

    banner

--- src/components/BannersHomeSections.tsx
+++ src/components/BannersHomeSections.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 
 type Props = {
-  banners: any;
+    banners: any;
 };
 
 const BASE_IMG = 'https://congresoedomex.gob.mx/';
@@ -11,7 +11,7 @@ export default function BannersHomeSection({ banners } : Props) {
         <div className="w-layout-grid grid-2">
             {banners?.slice().map((item:any, index: any) => (
                 <Link href={`${item?.url}`}>
-                    <img src={`${BASE_IMG}/${item?.fotos[0].path}`} loading="lazy" alt="" className="image-13 banner-info" />
+                    <img key={index} src={`${BASE_IMG}/${item?.fotos[0].path}`} loading="lazy" alt="" className="image-13 banner-info" />
                 </Link>
             ))
             }   
