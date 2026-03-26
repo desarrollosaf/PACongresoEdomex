7651bfad2ab146e5c53ac646b28b53a841fc8433
GiselDiaz <gis_ale13@hotmail.com>
2026-03-26 16:51:18 -0600

    limk

--- src/components/BannersHomeSections.tsx
+++ src/components/BannersHomeSections.tsx
@@ -8,8 +8,8 @@ export default function BannersHomeSection({ banners } : Props) {
     return (
         <div className="w-layout-grid grid-2">
             {banners?.slice().map((item: any, index: any) => (
-                <Link href={`${item?.url}`}>
-                    <img key={index} src={`${BASE_IMG}/${item?.fotos[0].path}`} loading="lazy" alt="" className="image-13 banner-info" />
+                <Link key={index} href={`${item?.url}`}>
+                    <img src={`${BASE_IMG}/${item?.fotos[0].path}`} loading="lazy" alt="" className="image-13 banner-info" />
                 </Link>
             ))
             }   
