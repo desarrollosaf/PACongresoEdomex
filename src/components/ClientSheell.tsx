dfc8f7abc11487b32cf2fc73604541a63baf04f8
cescDG <cesar199403@hotmail.com>
2026-09-09 09:07:30 -0600

    recdocente

--- src/components/ClientSheell.tsx
+++ src/components/ClientSheell.tsx
@@ -16,19 +16,21 @@ export default function ClientShell({
   const isConvocatoria = pathname.startsWith("/consulta");
   const isReconocimiento = pathname.startsWith("/reconocimientodocente");
   const isParlamentojuvenil = pathname.startsWith("/parlamentojuvenil");
+  const isReconocimientoDocente2026 = pathname.startsWith("/ReconocimientoDocente2026");
+  const isLandingStandalone = isConvocatoria || isReconocimiento || isParlamentojuvenil || isReconocimientoDocente2026;
   return (
     <>
-      {!isConvocatoria && !isReconocimiento &&  !isParlamentojuvenil &&  <Header />}
+      {!isLandingStandalone && <Header />}
 
       <main className="flex-1">
         {children}
       </main>
 
-      {!isConvocatoria && !isReconocimiento && !isParlamentojuvenil && <Footer />}
-      {!isConvocatoria && !isReconocimiento && !isParlamentojuvenil && <FeedbackWidget />}
+      {!isLandingStandalone && <Footer />}
+      {!isLandingStandalone && <FeedbackWidget />}
       {/* {!isConvocatoria && <FloatingChat />} */}
 
-      {!isConvocatoria && !isReconocimiento && !isParlamentojuvenil && (
+      {!isLandingStandalone && (
         <>
           <Script
             src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=68dd617a668359798a8993c6"
