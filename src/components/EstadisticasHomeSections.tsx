3b7b9ba7a8f37c5acd6906090736ffa2407560d7
MarSGlez <martin.sanchezg@hotmail.com>
2026-09-09 15:43:21 -0600

    cahnges estadisiticas

--- src/components/EstadisticasHomeSections.tsx
+++ src/components/EstadisticasHomeSections.tsx
@@ -17,7 +17,7 @@ export default function EstadisticasHomeSection({ estadisticas }: Props) {
         <div data-w-id="c1fc9fc7-cb97-4374-627a-4e26bfd7443a" className="div-block-5">
           {/* <div className="text-block color_oro">+</div> */}
           <div className="text-block">
-            850
+            853
             {/* {
               estadisticas.data.iniciativas.aprobadas +
               estadisticas.data.puntos_acuerdo.total +
@@ -37,7 +37,7 @@ export default function EstadisticasHomeSection({ estadisticas }: Props) {
       <div data-w-id="7bd2727f-f0fc-56ea-a56c-ce85e6db2e91" className="column numeralia_columna w-col w-col-3">
         <div data-w-id="c1fc9fc7-cb97-4374-627a-4e26bfd7443a" className="div-block-5">
           {/* <div className="text-block color_oro">+</div> */}
-          <div className="text-block">  {  estadisticas?.data?.totales_generales?.total_sesiones ?? 0}</div>
+          <div className="text-block"> 137 {/* {  estadisticas?.data?.totales_generales?.total_sesiones ?? 0} */}</div>
         </div>
         <div className="descripcion_dato">Sesiones</div>
       </div>
